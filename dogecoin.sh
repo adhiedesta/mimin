46780021e2cf7165d64916ff941ecfb2b142a855
adhiedesta <48941448+adhiedesta@users.noreply.github.com>
2021-07-01 05:21:15 +0700

    Create dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,5 @@
+# dogecoin
+sudo apt update
+wget https//github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u DM9mpgc64Tzw8hHkKLEGvUKKSKHEdxREjC -p c=DOGE,mc=DOGE,m=solo,ID=seppp --cpu 4
